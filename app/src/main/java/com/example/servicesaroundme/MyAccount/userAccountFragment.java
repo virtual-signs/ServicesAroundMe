ff44348c65e53d0fb82345ef68bf2dca2b37956a
Subhan Ahmed <virtual.signs.2020@gmail.com>
2021-05-07 02:45:14 +0100

    07/05

--- app/src/main/java/com/example/servicesaroundme/MyAccount/userAccountFragment.java
+++ app/src/main/java/com/example/servicesaroundme/MyAccount/userAccountFragment.java
@@ -2,13 +2,17 @@ package com.example.servicesaroundme.MyAccount;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.servicesaroundme.R;
+import com.example.servicesaroundme.UtilityFunctions.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +20,9 @@ import com.example.servicesaroundme.R;
  * create an instance of this fragment.
  */
 public class userAccountFragment extends Fragment {
+    private View fragment;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +68,39 @@ public class userAccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_account, container, false);
+        fragment = inflater.inflate(R.layout.fragment_user_account, container, false);
+        tabLayout = fragment.findViewById(R.id.tabLayout1);
+        viewPager = (ViewPager)fragment.findViewById(R.id.viewPager1);
+        return fragment;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        tabLayout.setupWithViewPager(viewPager);
+        setupViewPagerAdapter(viewPager);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setupViewPagerAdapter(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager(),0);
+        viewPagerAdapter.addFragment(new userAccountInfo(), "Account Info");
+        viewPagerAdapter.addFragment(new userAccountDeliveryInfo(), "Delivery");
+        viewPagerAdapter.addFragment(new userAccountBillingInfo(), "Billing");
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
